7630125ef3b8cce12f5b3dabc3f685effe28118e
Harisinh Makwana <webcmsexpert@gmail.com>
2024-09-22 01:49:38 +0530

    new plugin with readme

--- README.md
+++ README.md
@@ -1,3 +1,184 @@
-# Conzent CMP & IAB/TFC 2.2 Cookie Banner
-To configure the Conzent CMP you can either use GTM or a single line of Javascript code.
-Please refer to the https://conzent.net/installation-guide/ for more information
+=== Conzent - Cookie Banner - Conzent CMP - Google CMP & IAB TCF Certified ===
+Contributors: conzent
+Donate link: https://conzent.net/ 
+Tags: GDPR, CCPA, cookie notice, DSGVO, cookie consent, cookie banner, eu cookie law, conzent, eprivacy, gdpr consent
+Requires at least: 5.0.0
+Tested up to: 6.6
+Requires PHP: 5.6
+Stable tag: 1.0.2
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Easily set up cookie banner or cookie notice and cookie policy page for GDPR (DSGVO, RGPD) compliance. Also supports CCPA/CPRA and other major global laws.
+
+== Description ==
+
+Conzent CMP WordPress Cookie Banner and Cookie Policy generator. IAB/TCF and Google CMP Certified - Comply with the major data protection laws (GDPR, ePrivacy, CCPA, LGPD, etc.)
+
+== Key Features == 
+
+– **Automatic scanning**: Single-click automatic scanning and categorization of cookies.
+
+– **Cookie auto-blocking**: Automatically block non-essential cookies to ensure GDPR compliance.
+
+– **Cookie list**: This lets you display the list of cookies neatly in a table on your Privacy & Cookies Policy page by using a shortcode. You can also add new cookies manually, edit existing ones (CookieID, Category, Description, Script URL Pattern), and delete cookies as needed.
+
+– **Preference center**: Offer a second layer of control through a preference center within the banner. Allow site visitors to provide granular consent for specific cookie categories, giving them the choice to opt in or opt-out.
+
+– **Revisit consent button**: Visitors can either permanently dismiss the cookie banner or access it later through a revisit consent button, which can be tailored in terms of color and position.
+
+– **Google Consent Mode support**: Implement Google Consent Mode v2 easily to keep ads effective.
+
+– **Auto-translation**: Auto-translate the banner in 30+ languages as per the user’s browser language.
+
+– **Integration with Conzent Web App**: Free connection with Conzent web app to access advanced features (cookie scan, consent log, etc.) and manage all settings from the web app account.
+
+
+
+== Additional Features == 
+
+
+
+
+== The Conzent GDPR Cookie Consent Plugin Uses The Following Cookie Only ==
+
+– "conzentConsent" – Conzent sets this cookie to remember users' consent preferences so that their preferences are respected on their subsequent visits to this site. It does not collect or store any personal information of the site visitors.
+
+
+== Setup Guide ==
+
+Our [WordPress cookie plugin user guide](https://conzent.net/installation-guide/#plugins "Help - Installation Guide") will help you set up the plugin on your website.
+
+
+== Useful links ==
+
+[Plugin FAQ](https://conzent.net/pricing/#faq)
+[GDPR cookie consent](https://conzent.net/understanding-gdpr-in-a-nutshell/)
+[Cookie Law](https://conzent.net/the-complete-guide-to-ccpa-compliance/)
+[Cookie banner guide](https://conzent.net/the-complete-guide-to-cookie-banners/)
+
+== Conzent Cookie banner plugin premium features ==
+
+Access the following features by subscribing to a premium plan (Single, Pro, or Enterprise) on the Conzent web app account. [See all features and compare plans](https://conzent.net/pricing/)
+
+&#9989; **Increased pageviews limit**: Increase your pageviews limit, extending from 100,000 to unlimited. </li>
+&#9989; **Enhanced scanning capacity**: Our tool can scan up to 8,000 pages in a single scan. </li>
+&#9989; **GDPR/US laws template**: Offer opt-in consent banner for GDPR and opt-out banner for US privacy laws such as CCPA/CPRA. </li>
+&#9989; **Global Privacy Control**: Respect the user’s GPC setting if enabled. </li>
+&#9989; **TCF v2.2 integration**: We seamlessly integrate with TCF v2.2, the IAB Europe Consent Framework for Publishers. Our registered CMP ID is 401. </li>
+&#9989; **Advanced customization**: Customize your banner with advanced design options such as custom CSS to suit your brand's identity. </li>
+&#9989; **Popup layout**: Add a cookie notification in the form of a ‘Popup’. </li>
+&#9989; **Geo-targeting banner**: Target your cookie banner based on location, including GDPR banner for Worldwide, EU & UK, and Other countries, as well as CCPA/CPRA banner for Worldwide, United States, and Other countries. </li>
+&#9989; **Scheduled scans**: Set up scans on a schedule that works for you. </li>
+&#9989; **Custom branding**: Add your brand logo to the cookie banner for a personalized touch. </li>
+
+= NOTE: INSTALLING THIS PLUGIN ALONE DOES NOT MAKE YOUR SITE GDPR AND CCPA COMPLIANT. SINCE EACH SITE USES DIFFERENT COOKIES, YOU NEED TO ENSURE YOU HAVE THE NECESSARY CONFIGURATIONS IN PLACE WITH A SUPPORTING COOKIE NOTICE. = 
+
+
+== Frequently Asked Questions ==
+
+= The cookie notice header doesn’t work on my browser =
+Please report a bug on the support forum. Make sure to include the following information:
+
+* Your URL (We need it to help you!)
+* WordPress version (found in the bottom right-hand corner of the dashboard)
+* Browser where the issue is observed (e.g., Firefox, Chrome, IE)
+* Describe the problem. The more information you provide, the faster we can respond.
+
+
+= What does this plugin do? =
+This WordPress cookie plugin will:
+
+* Add a cookie banner to the top/bottom of all pages on your website, notifying visitors that you have a clearly defined privacy and cookie policy.
+* Allow you to record which cookies your site uses and neatly display a list of these cookies. You can add this list to your Privacy Policy page or Cookie Policy page using a shortcode.
+Note: If you connect your site to the Conzent web app, you have to replace the shortcode with an HTML code.
+
+= What is a cookie notice? =
+A cookie notice, also known as a cookie banner, is a message informing visitors about the site's use of cookies. It typically explains what cookies are, why they are used, and how they affect user privacy. It often includes options for users to consent to or manage cookie preferences, ensuring compliance with privacy laws like the GDPR and CCPA. The goal is to provide transparency and allow users to make informed decisions about their data.
+
+= Do visitors now have to accept/refuse cookies in order to use websites? =
+No, Only the ‘Necessary’ cookies will be loaded until the user gives consent. 
+
+= Does this plugin stop cookies from being stored? =
+Visitors can reject the category of cookies they don't want to be installed. In such cases, all cookies in that category will be blocked for the user.
+
+But if another plugin adds scripts or places a cookie, we have no control. We have no way of knowing what plugins you use or how they work, so blocking/deleting cookies on a WordPress website/blog might possibly/probably break your site. Larger websites with substantial budgets may well invest in such technology, but this plugin aims to help you provide a certain level of compliance without taking drastic action.
+
+= Does this plugin guarantee that I comply with GDPR? =
+This plugin can be used to comply with the cookie consent requirements of major data privacy laws such as the GDPR. However, you need to assess your website's use of cookies to confirm that it aligns with GDPR guidelines. If you are seeking specialist legal advice regarding your website, we suggest that you consult a lawyer to understand how you can achieve full GDPR compliance.
+
+= What is GDPR? =
+GDPR, or the General Data Protection Regulation is a comprehensive data protection law in the EU aimed at safeguarding EU individuals' privacy and regulating how organizations handle their personal data. It is also known as DatenSchutz-Grundverordnung or DSGVO in German.
+
+= What is CCPA? =
+CCPA is the California Consumer Privacy Act, a law granting California residents more control over their personal information held by businesses. Businesses must be transparent, offer opt-out options, and ensure data security.
+
+= What is GDPR cookie consent? =
+GDPR requires websites to obtain explicit consent from users before storing or accessing cookies on their devices. This is known as GDPR cookie consent.
+
+GDPR mandates cookie consent because cookies allow websites to collect data and track users across multiple sessions. Since this impacts user privacy, consent is required before enabling cookies, especially non-essential cookies like analytics, advertisements, etc.
+
+= What is CCPA compliance? =
+CCPA Compliance refers to adherence to the California Consumer Privacy Act (CCPA), a law designed to protect the privacy rights of California residents. It mandates that businesses handling the personal information of California residents provide certain rights and protections to those individuals. Compliance involves ensuring that businesses meet the CCPA's requirements regarding data collection and processing.
+
+= Does the plugin block cookies before consent? =
+Non-essential cookies are automatically blocked until explicit consent is given by users. Essential or strictly necessary cookies are allowed by default to ensure site functionality.
+
+= Can I export or log user consent as per GDPR requirements? =
+Yes, you can export a log of all user cookie consent and preferences related to cookies. This helps meet GDPR compliance needs.
+
+= Can the plugin help me generate a GDPR-compliant cookie policy? =
+Yes, we provide an easy wizard to customize and generate a GDPR-compliant cookie policy based on your scanned site cookies.
+
+== Installation ==
+
+= Automatic installation =
+
+– Go to **Plugins** from your WordPress dashboard and select **Add New Plugin**.
+
+– Search for “Conzent Cookie Banner” and **Install Now**.
+
+– Click **Activate** to enable the plugin.
+
+= Manual installation =
+
+– Download the Conzent Cookie Banner plugin zip file from the WordPress.org page.
+
+– Go to **Plugins** from your WordPress dashboard and select **Add New Plugin**.
+
+– Click **Upload Plugin** and choose the downloaded zip file.
+
+– Click **Install Now** and after installation, click **Activate Plugin**.
+
+Alternatively, you can upload the zip using the FTP application. Extract the zip file and upload the folder to your **/wp-content/plugins/** directory. Once uploaded, activate the plugin from your WordPress dashboard.
+
+= To connect the plugin to the Conzent web app: =
+
+
+– After activating the plugin, it will automatically verify domain and setup adavanced setting from web app account.
+
+= Plugin updates =
+
+Every time we update the plugin, you will see a notification on the installed plugins page. You can directly update the plugin from the WordPress dashboard. We recommend keeping the plugin updated to avail new functionalities and security features.
+
+
+== Screenshots ==
+
+1. Cookie consent banner web setting on the user-end
+
+
+== Changelog ==
+
+= 1.0.2 =
+[Compatibility] – Tested OK with WordPress version 6.6
+* Improved design & appearance
+
+
+= 1.0.0 =
+* First public release.
+
+== Upgrade Notice ==
+
+= 1.0.2 =
+
+[Compatibility] – Tested OK with WordPress version 6.6
---
+++ conzent.php
@@ -0,0 +1,303 @@
+<?php
+/**
+* @link            https://conzent.net/
+*
+* @wordpress-plugin
+* Plugin Name: Conzent - Cookie Banner - Conzent CMP - Google CMP & IAB TCF Certified
+* Plugin URI: https://conzent.net/
+* Description: Conzent CMP WordPress Cookie Banner and Cookie Policy generator. IAB/TCF and Google CMP Certified - Comply with the major data protection laws (GDPR, ePrivacy, CCPA, LGPD, etc.)
+* Author: Conzent ApS
+* Version: 1.0.2
+* Author URI: https://conzent.net/
+* License:           GPLv3
+* License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+* Text Domain: conzent
+**/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;// Abort if this file is accessed directly.
+}
+
+add_action('wp_enqueue_scripts', 'cnzbannerfront');
+add_action('admin_enqueue_scripts','cnzbanneradmin');
+add_action('init', 'cnz_banner_register_hooks');
+add_action('init', 'cnz_banner_add_shortcode');
+add_action('admin_menu', 'cnz_add_menu_items');
+add_action('plugins_loaded', 'cnz_update_check');
+add_action('activated_plugin', 'cnz_save_activation_error');
+add_action('wp_body_open','add_cnz_gtm_after_body');
+/** Conzent web app URL */
+if ( ! defined( 'CNZ_APP_URL' ) ) {
+	define( 'CNZ_APP_URL', 'https://conzent.net/app' );
+}
+
+/** Conzent web app script  URL. */
+if ( ! defined( 'CNZ_APP_API_URL' ) ) {
+	define( 'CNZ_APP_API_URL', 'https://conzent.net/app/api/v1' );
+}
+function cnz_update_check() {
+	if (!function_exists('get_plugin_data')) {
+         require_once ABSPATH . 'wp-admin/includes/plugin.php';
+     }
+	$plugin_data = get_plugin_data( __FILE__ );
+	$plugin_version = $plugin_data['Version'];
+	$conzent_db_version = $plugin_version;
+
+	if (get_site_option( 'conzent_db_version' ) != $conzent_db_version) {
+		update_option('conzent_db_version', $conzent_db_version);
+    }
+}
+function cnz_save_activation_error()
+{
+    $error = ob_get_contents();
+    if (!empty($error)) {
+        update_option('conzent_plugin_error', $error);
+    }
+}
+function cnz_activate() {
+
+  	if (get_option('conzent_db_version') === false) {
+		//Install
+		cnz_callback('install');
+	} else {
+		//Update
+		cnz_callback('update');
+	}
+	
+	$plugin_data = get_plugin_data( __FILE__ );
+	$plugin_version = $plugin_data['Version'];
+	$conzent_db_version = $plugin_version;
+	update_option('conzent_db_version', $conzent_db_version);
+
+  /* activation code here */
+}
+register_activation_hook( __FILE__, 'cnz_activate' );
+function cnzbannerfront()
+{
+	if (!is_admin()) {
+		wp_enqueue_style('cnz-banner-css', plugins_url('assets/css/conzent-banner.css', __FILE__), false, false, 'screen');
+		wp_enqueue_script('cnz-banner-js',plugins_url('assets/js/conzent-banner.js', __FILE__),false, false,true);
+	}
+}
+function cnzbanneradmin()
+{	
+	if (is_admin()) {
+		wp_enqueue_style('cnz-banner-admin-css', plugins_url('assets/css/conzent-banner-admin.css', __FILE__), false, false, 'screen');
+	}
+}
+
+function cnz_callback($action)
+{
+	if($action == 'install'){
+		$site_info = cnz_verifyDomain();
+		if(!empty($site_info) && array_key_exists("domain",$site_info)){
+			add_option( 'conzent_website_key',$site_info['website_key'] );
+			add_option( 'conzent_site_name', $site_info['site_name'] );
+			add_option( 'conzent_site_domain', $site_info['domain'] );
+			add_option( 'conzent_site_status', $site_info['status'] );
+			add_option( 'conzent_site_id', $site_info['id'] );
+		}
+	}
+	else if($action == 'update')
+	{
+        cnz_update_check();
+	}
+}
+function cnz_banner_register_hooks(){
+	if (!is_admin()) {
+		add_action( 'wp_head','add_cnz_js', - 9998 );
+		add_action( 'wp_head','add_cnz_gtm_js', - 9996 );
+	}
+}
+function add_cnz_js(){
+	echo __('<script id="conzentbanner" type="text/javascript" src="'.CNZ_APP_URL.'/app/sites_data/'.get_option( 'conzent_website_key' ).'/script.js"></script>');
+}
+function cnz_add_menu_items() {
+
+	$hook = add_menu_page(
+		__( 'Conzent Banner', 'conzent' ), // Page title.
+		__( 'Conzent Banner', 'conzent' ),        // Menu title.
+		'activate_plugins',                                         // Capability.
+		'conzent',                                             // Menu slug.
+		'cnz_banner_setting',                                       // Callback function.
+		plugin_dir_url(__FILE__) . 'assets/images/logo_icon.png',
+		90
+	);
+	add_submenu_page( 'conzent', __( 'Setting', 'conzent' ), __( 'Setting', 'conzent' ), 'manage_options', 'cnz_banner_setting', 'cnz_setting_actions',1 );
+	
+}
+function cnz_banner_setting() {
+	if(isset($_GET['refresh'])){
+		cnz_get_siteinfo();
+		echo __('<script>window.location = "'.admin_url('admin.php?page=conzent').'"</script>');
+		exit;
+	}
+	else{
+	?>
+    <div class="opt_welcome"><h2><img src="<?php echo plugin_dir_url(__FILE__) . 'conzent-logo.png';?>" height="35px" />&nbsp;<?php echo esc_html__('Welcome to Conzent Banner','conzent');?></h2></div>
+    <div class="opt_box_welcome">
+    <div class="opt_item">
+    	<div class="opt_key"><?php echo esc_html__('Website Key :','conzent');?></div>
+        <div class="opt_val"> <?php echo get_option( 'conzent_website_key');?></div>
+    </div>
+    <!--<div class="opt_item">
+    	<div class="opt_key"><?php echo esc_html__('Website Id :','conzent');?></div>
+        <div class="opt_val"> <?php echo get_option( 'conzent_site_id');?></div>
+    </div>-->
+    <div class="opt_item">
+    	<div class="opt_key"><?php echo esc_html__('Site Name:','conzent');?></div>
+        <div class="opt_val"> <?php echo get_option( 'conzent_site_name');?></div>
+     </div>
+    <div class="opt_item">
+    	<div class="opt_key"><?php echo esc_html__('Domain:','conzent');?></div>
+        <div class="opt_val"> <?php echo get_option( 'conzent_site_domain');?></div>
+    </div>
+    <div class="opt_item">
+    	<div class="opt_key"><?php echo esc_html__('Status:','conzent');?></div>
+        <div class="opt_val"> <?php echo (get_option( 'conzent_site_status') == 1 ? 'Active':'Inactive');?></div>
+    </div>
+    <div style="margin:10px 0px;"><a href="<?php echo admin_url('admin.php?page=conzent&refresh=1')?>" class="cnz-btn"><?php echo esc_html__('Refresh','conzent');?></a></div>
+	<div style="margin-top:20px;margin-bottom:20x;"><a href="<?php echo CNZ_APP_URL;?>" class="cnz-btn-normal"><?php echo esc_html__('All settings is done in the conzent.net/app','conzent');?></a></div>
+    </div>
+    
+    <?php
+	}
+}
+function cnz_setting_actions()
+{
+
+$current_user = wp_get_current_user();
+	$is_admin=false;
+	if(in_array('administrator',$current_user->roles)){
+		$is_admin=true;
+	}
+	if(!$is_admin){
+		$redirect_url = admin_url('admin.php?page=conzent');
+		echo __('<script>window.location = "'.esc_url($redirect_url).'"</script>');
+		exit;
+	}
+$msg ='';	
+if(isset($_POST['action']) && $_POST['action']=='savesetting'){
+	update_option('conzent-gtm-id',$_POST['conzent_gtm_id']);
+	update_option('conzent-data-layer',$_POST['conzent_data_layer']);
+	$msg = esc_html__('Setting saved successfully','conzent');
+	
+}
+$conzent_gtm_id=get_option('conzent-gtm-id','');
+$conzent_data_layer=get_option('conzent-data-layer','');
+$conzent_site_id  = get_option( 'conzent_site_id');
+$conzent_website_key  = get_option( 'conzent_website_key');
+
+
+	?>
+    <div class="opt_welcome"><h2><img src="<?php echo plugin_dir_url(__FILE__) . 'conzent-logo.png';?>" height="35px" />&nbsp;<?php echo esc_html__('Conzent Banner Setting','conzent');?></h2></div>
+    <div class="opt_box_setting">
+    <div><?php if($msg){ echo '<div class="cnz-success">'.$msg.'</div>';}?></div>
+    	<form method="post" action="" name="frmsetting">
+        <!--<div class="opt_item">
+          		<div class="opt_key"><?php echo esc_html__('Website ID :','conzent');?> <span style="font-weight:normal;"><?php echo $conzent_site_id;?></span></div>
+         	</div>
+		--> 
+         <div class="opt_item">
+          <div class="opt_key"><?php echo esc_html__('Website Key :','conzent');?> <span style="font-weight:normal;"><?php echo $conzent_website_key;?></span></div>
+          
+         </div>
+        <div class="opt_item">
+          <div class="opt_key"><?php echo esc_html__('Google Tag Manager ID :','conzent');?></div>
+           <div class="opt_val">
+            <input type="text" name="conzent_gtm_id" id="conzent_gtm_id" value="<?php echo $conzent_gtm_id;?>" style="min-width:300px;"/>
+          </div>
+         </div>
+         <div class="opt_item"> 
+          <div class="opt_key"><?php echo esc_html__('Google Tag Data layer :','conzent');?></div>
+           <div class="opt_val">
+            <input type="text" name="conzent_data_layer" id="conzent_data_layer" value="<?php echo $conzent_data_layer;?>" placeholder="dataLayer" style="min-width:300px;"/>
+          </div>
+         </div>
+          <div class="action_box">
+            <input type="hidden" name="action" value="savesetting" />
+            <input type="submit" name="savesett" value="Save" class="cnz-btn"/>
+          </div>
+        </form>
+        <div style="margin-top:20px;margin-bottom:20x;"><a href="<?php echo CNZ_APP_URL;?>" class="cnz-btn-normal"><?php echo esc_html__('All settings is done in the conzent.net/app','conzent');?></a></div>
+    </div>
+<?php
+}
+function cnz_get_siteinfo(){
+	$site_info = cnz_verifyDomain();
+	  if(!empty($site_info) && array_key_exists("domain",$site_info)){
+		update_option( 'conzent_website_key',$site_info['website_key'] );
+		update_option( 'conzent_site_name', $site_info['site_name'] );
+		update_option( 'conzent_site_domain', $site_info['domain'] );
+		update_option( 'conzent_site_status', $site_info['status'] );
+		update_option( 'conzent_site_id', $site_info['id'] );
+	  }
+}
+function cnz_banner_add_shortcode(){
+	add_shortcode('CONZENT_CONSENT_ID', 'cnz_consent_id');
+	add_shortcode('conzent_consent_id', 'cnz_consent_id');
+}
+function cnz_consent_id($atts, $content) {
+	 global $post;
+  	$result_content = '';
+	$default = array();
+    $short_arr = shortcode_atts($default, $atts);
+	$result_content = '
+	<div class="cnz-tracking-box">
+		<div class="tracking-inner">
+			<div class="cnz-label">Conzent Consent ID</div>
+			<div class="cnz-val"><span id="conzentId"></span></div>
+		</div>
+	</div>';
+	return $result_content;
+}
+function cnz_verifyDomain() {
+ 	$items = array();
+	$api_url = CNZ_APP_API_URL."/verify?domain=".sanitize_key($_SERVER['SERVER_NAME']);
+	$response_obj = wp_remote_get($api_url);
+	$http_code = wp_remote_retrieve_response_code( $response_obj );
+	
+	if($http_code == 200){
+		$items = json_decode(wp_remote_retrieve_body( $response_obj ),true);
+	}
+	return $items;
+}
+function add_cnz_gtm_after_body() { 
+  if(get_option( 'conzent-gtm-id')){
+  ?>
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo esc_js( get_option( 'conzent-gtm-id' ) ); ?>"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+  <?php
+  }
+}
+function add_cnz_gtm_js(){
+
+	if ( empty( get_option( 'conzent-data-layer' ) ) ) {
+		$data_layer = 'dataLayer';
+	} else {
+		$data_layer = get_option( 'conzent-data-layer' );
+	}
+	if(get_option( 'conzent-gtm-id')){
+?>
+	<!-- Google Tag Manager -->
+	<script>
+        (function (w, d, s, l, i) {
+            w[l] = w[l] || []; w[l].push({'gtm.start':new Date().getTime(), event: 'gtm.js'});
+            var f = d.getElementsByTagName(s)[0],  j = d.createElement(s), dl = l !== 'dataLayer' ? '&l=' + l : '';
+            j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);})(
+            window,
+            document,
+            'script',
+            '<?php echo esc_js( $data_layer ); ?>',
+            '<?php echo esc_js( get_option( 'conzent-gtm-id' ) ); ?>'
+        );
+    </script>
+    <!-- End Google Tag Manager -->
+    <?php
+    }
+}
+
+?>
