3ce95cedeb0db05ffd7f384b9c78a04a904f3b70
Conzent-net <info@conzent.net>
2024-10-10 18:00:18 +0200

    Update conzent.php
    Removed author uri as it must be different from plugin uri

--- conzent.php
+++ conzent.php
@@ -6,7 +6,6 @@
 * Plugin Name: Conzent - Cookie Banner - Conzent CMP - Google CMP & IAB TCF Certified
 * Plugin URI: https://conzent.net/
 * Description: Conzent CMP WordPress Cookie Banner and Cookie Policy generator. IAB/TCF and Google CMP Certified - Comply with the major data protection laws (GDPR, ePrivacy, CCPA, LGPD, etc.)
-* Author: Conzent ApS
 * Version: 1.0.4
 * Author URI: https://conzent.net/
 * License:           GPLv3
